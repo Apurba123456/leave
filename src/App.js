df7df3cc3d1c6235a82a3b7f8874130270fa38e6
Apurba123456 <apurbawin8@gmail.com>
2024-09-05 22:19:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import  Leave  from './components/Leave';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Leave/>
+    </div>
+  );
+}
+
+export default App;
+
